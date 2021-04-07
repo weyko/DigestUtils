5e53987adfae063db8ef22dd2ac88bec45c38065
zhongxiwei <zhong_xiwei@sina.com>
2021-04-07 15:14:33 +0800

    first version

---
+++ digest/src/main/java/org/springframework/util/DigestUtils.java
@@ -0,0 +1,50 @@
+package org.springframework.util;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * TODO : 数字加密工具类
+ * auther: weyko
+ * create: 2021/4/7 10:58
+ */
+public class DigestUtils {
+    private static final String MD5_ALGORITHM_NAME = "MD5";
+    private static final char[] HEX_CHARS =
+            {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+    public static String md5DigestAsHex(byte[] bytes) {
+        return digestAsHexString(MD5_ALGORITHM_NAME, bytes);
+    }
+    private static String digestAsHexString(String algorithm, byte[] bytes) {
+        char[] hexDigest = digestAsHexChars(algorithm, bytes);
+        return new String(hexDigest);
+    }
+    private static char[] digestAsHexChars(String algorithm, byte[] bytes) {
+        byte[] digest = digest(algorithm, bytes);
+        return encodeHex(digest);
+    }
+    private static byte[] digest(String algorithm, byte[] bytes) {
+        return getDigest(algorithm).digest(bytes);
+    }
+    private static char[] encodeHex(byte[] bytes) {
+        char chars[] = new char[32];
+        for (int i = 0; i < chars.length; i = i + 2) {
+            byte b = bytes[i / 2];
+            chars[i] = HEX_CHARS[(b >>> 0x4) & 0xf];
+            chars[i + 1] = HEX_CHARS[b & 0xf];
+        }
+        return chars;
+    }
+    /**
+     * Creates a new {@link MessageDigest} with the given algorithm. Necessary
+     * because {@code MessageDigest} is not thread-safe.
+     */
+    private static MessageDigest getDigest(String algorithm) {
+        try {
+            return MessageDigest.getInstance(algorithm);
+        }
+        catch (NoSuchAlgorithmException ex) {
+            throw new IllegalStateException("Could not find MessageDigest with algorithm \"" + algorithm + "\"", ex);
+        }
+    }
+}
